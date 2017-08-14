07f054fb18d69788360b982ea15a72fb89558f02
PalOne <p410n3@gmail.com>
2017-08-14 22:07:35 +0200

    install.sh should work now

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#frk1 stuff
 cd /var/
 wget --no-check-certificate "https://nodejs.org/dist/latest/node-$(curl -L 'nodejs.org/dist/index.tab' | sed -n '2p' | awk '{ print $1 }')-linux-x64.tar.gz" -O /tmp/nodejs.tar.gz
 sudo tar --strip-components 1 -xzvf /tmp/nodejs.tar.gz -C /usr/local
@@ -9,12 +10,15 @@ git clone https://github.com/frk1/steamhourboostv2.git
 cd steamhourboostv2
 npm install .
 coffee user.coffee
-cd ..
-mkdir steam_sudo_wrapper
-cd steam_sudo_wrapper
-#download .sh stuff
+cd /var/
+#download git repo
+git clone https://github.com/p410n3/SteamHourBoostWebInterface.git
+cd SteamHourBoostWebInterface
+rm install.sh
+mv ./steam_sudo_wrapper ../
+#allow www-data to run the scripts
 echo 'www-data ALL = NOPASSWD: /var/steam_sudo_wrapper/start.sh' | sudo EDITOR='tee -a' visudo
 echo 'www-data ALL = NOPASSWD: /var/steam_sudo_wrapper/stop.sh' | sudo EDITOR='tee -a' visudo
-#download webstuff
-echo "You will now have a folder named 'COPY_AND_RENAME_ME' in /var/. Copy that into your web directory and rename it."
+#end
+echo "You will now have a folder '/var/SteamHourBoostWebInterface/interface'. Copy that into your web directory and rename it."
 read -p "Press any key to finish."
