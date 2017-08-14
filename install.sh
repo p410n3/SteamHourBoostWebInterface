4093ff355f21c1b7e918b31bc14538c6033911a8
PalOne <p410n3@gmail.com>
2017-08-14 22:20:31 +0200

    fix install.sh#2

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+cd /var/
+#download my git repo
+git clone https://github.com/p410n3/SteamHourBoostWebInterface.git
+cd SteamHourBoostWebInterface
+rm install.sh
+mv ./steam_sudo_wrapper ../
+#allow www-data to run the scripts
+echo 'www-data ALL = NOPASSWD: /var/steam_sudo_wrapper/start.sh' | sudo EDITOR='tee -a' visudo
+echo 'www-data ALL = NOPASSWD: /var/steam_sudo_wrapper/stop.sh' | sudo EDITOR='tee -a' visudo
+#end
+echo "You will have a folder '/var/SteamHourBoostWebInterface/interface'. Copy that into your web directory and rename it."
+read -p "Press any key to continue."
 #frk1 stuff
 cd /var/
 wget --no-check-certificate "https://nodejs.org/dist/latest/node-$(curl -L 'nodejs.org/dist/index.tab' | sed -n '2p' | awk '{ print $1 }')-linux-x64.tar.gz" -O /tmp/nodejs.tar.gz
@@ -9,16 +21,3 @@ sudo pm2 install coffeescript
 git clone https://github.com/frk1/steamhourboostv2.git
 cd steamhourboostv2
 npm install .
-cd /var/
-#download git repo
-git clone https://github.com/p410n3/SteamHourBoostWebInterface.git
-cd SteamHourBoostWebInterface
-rm install.sh
-mv ./steam_sudo_wrapper ../
-#allow www-data to run the scripts
-echo 'www-data ALL = NOPASSWD: /var/steam_sudo_wrapper/start.sh' | sudo EDITOR='tee -a' visudo
-echo 'www-data ALL = NOPASSWD: /var/steam_sudo_wrapper/stop.sh' | sudo EDITOR='tee -a' visudo
-#end
-echo "You will now have a folder '/var/SteamHourBoostWebInterface/interface'. Copy that into your web directory and rename it."
-read -p "Press any key to start the User Config."
-coffee user.coffee
