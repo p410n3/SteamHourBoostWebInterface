3e54d7d2427960e50a40eb08ef39fb9a58b210d4
PalOne <p410n3@gmail.com>
2017-08-14 22:46:19 +0200

    password = password

--- interface/config.php
+++ interface/config.php
@@ -2,4 +2,4 @@
 /* Define username and password */
 $Username = "palone";
 /*  Use "http://www.passwordtool.hu/php5-password-hash-generator" because the one from the OG github is total shit  */
-$Password = "$2y$10$.j0e/GfkrK7UYitb5QT4Le2s65HnsXuWDZjrAD.XPTWhvnsrFPBe2";
+$Password = "$2y$10$oEBNufIYe.VlqSJHnHCX/OBqNL.99YbL3ohvaVuYNF3yJMNliGYPe";
