d295a923749508270ab7de84278e6316b1d09b7f
Phillip L <p410n3@gmail.com>
2017-08-24 09:08:43 +0200

    sec#4

--- interface/start.php
+++ interface/start.php
@@ -1,4 +1,6 @@
 <?php
+  session_start(); /* Starts the session */
+
   if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in */
     header("location:login.php");
 	  exit;
