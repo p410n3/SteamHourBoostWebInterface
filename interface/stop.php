cb180a75ca8dae13bc8e0d1d88ecf7b0beaf3cb4
Phillip L <p410n3@gmail.com>
2017-08-24 09:09:00 +0200

    sec#4

--- interface/stop.php
+++ interface/stop.php
@@ -1,10 +1,12 @@
 <?php
+  session_start(); /* Starts the session */
+
   if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in */
     header("location:login.php");
 	  exit;
 }
 	  
-    exec('sudo -u root /var/steam_sudo_wrapper/stop.sh');
+    exec('sudo -u root /var/steam_sudo_wrapper/start.sh');
 	sleep(3);
 	header("Location: ./index.php");
 ?>
