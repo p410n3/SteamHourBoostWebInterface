f3d9f3a2b1a208636f1f7a2ef8bc0f25fde6ef60
Phillip L <p410n3@gmail.com>
2017-08-24 08:58:06 +0200

    security update

--- interface/start.php
+++ interface/start.php
@@ -1,4 +1,7 @@
 <?php
+require_once ('config.php'); // For storing username and password.
+session_start();
+
     exec('sudo -u root /var/steam_sudo_wrapper/start.sh');
 	sleep(3);
 	header("Location: ./index.php");
