c743b687ce81e433eaab27096a20f96c89c324af
Phillip L <p410n3@gmail.com>
2017-08-24 09:06:38 +0200

    sec#3

--- interface/start.php
+++ interface/start.php
@@ -1,9 +1,9 @@
 <?php
- session_start(); /* Starts the session */
   if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in */
     header("location:login.php");
 	  exit;
-
+}
+	  
     exec('sudo -u root /var/steam_sudo_wrapper/start.sh');
 	sleep(3);
 	header("Location: ./index.php");
