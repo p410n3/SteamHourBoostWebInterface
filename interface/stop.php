2ab02a6a364bf7d7830be1355541b7be57c5cd06
Phillip L <p410n3@gmail.com>
2017-10-23 22:35:39 +0200

    Update stop.php

--- interface/stop.php
+++ interface/stop.php
@@ -6,7 +6,7 @@
 	  exit;
 }
 	  
-    exec('sudo -u root /var/steam_sudo_wrapper/start.sh');
+    exec('sudo -u root /var/steam_sudo_wrapper/stop.sh');
 	sleep(3);
 	header("Location: ./index.php");
 ?>
